453d612e434e7adb4557f3918c65b454a7416ce0
郑伟 <whoyoulove2003@163.com>
2018-12-29 16:43:35 +0800

    代码修改

--- microservice-springcloud/microservice-springcloud-springboot/src/main/java/org/qingfox/microservice/springcloud/springboot/Test.java
+++ microservice-springcloud/microservice-springcloud-springboot/src/main/java/org/qingfox/microservice/springcloud/springboot/Test.java
@@ -1,5 +1,15 @@
 package org.qingfox.microservice.springcloud.springboot;
 
-public class Test {
+import java.io.IOException;
 
+import org.qingfox.framework.common.utils.ThreadUtil;
+import org.qingfox.framework.microservice.IServer;
+
+public class Test {
+  public static void main(String[] args) throws IOException {
+    IServer server = new SpringBootServer();
+    server.setPort(8080);
+    server.setScanPackage("org.qingfox.microservice");
+    server.start();
+  }
 }
